d48abf5fd8f0f2d71be496d70ad319efe8f890fc
Akash <akash.soedamah@gmail.com>
2020-02-20 09:36:16 +0100

    Imported SVG Logo (instead of via .css file)

--- src-web/src/components/frame/frame.js
+++ src-web/src/components/frame/frame.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
+import Logo from '../../fitworld-icon.svg'
+
+const FitWorldLogo = styled(Logo)`
+    margin-left: 10px;
+    width: 60px;
+    height: 70px;
+    display: flex;
+    align-items: center;
+    `
 
 class Frame extends Component {
   render() {
     return (
-        <div className="title-bar">
-          <div className="app-name-container" />
-          <div className="window-controls-container">
-            <button id="minimize-button" className="minimize-button" />
-            <button id="min-max-button" className="min-max-button" />
-            <button id="close-button" className="close-button" />
-          </div>
+      <div className="title-bar">
+        <FitWorldLogo/>
+        <div className="window-controls-container">
+          <button id="minimize-button" className="minimize-button"/>
+          <button id="min-max-button" className="min-max-button"/>
+          <button id="close-button" className="close-button"/>
+        </div>
       </div>
     )
   }
