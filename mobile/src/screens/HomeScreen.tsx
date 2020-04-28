df588df232f4f212847c1275c31add950239052a
Alpakash <akash.soedamah@gmail.com>
2020-04-28 16:45:49 +0200

    Switch component width based on StyledButton component width. StyledButton component width adjusted by text

--- mobile/src/screens/HomeScreen.tsx
+++ mobile/src/screens/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, ScrollView, StatusBar, View } from 'react-native'
+import { Button, ScrollView, StatusBar,Text } from 'react-native'
 import AuthGet from '../AuthGet'
 import AsyncStorage from "@react-native-community/async-storage"
 import Toggle from "../components/Toggle";
@@ -10,9 +10,13 @@ const HomeScreen = () => {
             <StatusBar backgroundColor="orange"/>
             <Button title={ 'clear the cache' } onPress={ () => AsyncStorage.clear() }/>
             <AuthGet/>
-            <Toggle/>
-
-
+            <Toggle>
+                <Text>hiiiii</Text>
+                <Text>hiiiiiiiiiiiiiiiiiiiii</Text>
+                <Text>hi</Text>
+                <Text>hi</Text>
+                <Text>hihihihihihihihi</Text>
+            </Toggle>
         </ScrollView>
     )
 };
